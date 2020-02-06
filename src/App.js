38e1b07dccfd152fc7b00b84fe1e2f7d8390e4b8
Edgar <exelcior99@gmail.com>
2020-02-06 10:23:45 +0900

    redux boilerplate and login application

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import createTheme from "@material-ui/core/styles/createMuiTheme";
 import themeFile from "./util/theme";
 import jwtDecode from "jwt-decode";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 // Components
 import Navbar from "./components/Navbar";
 import AuthRoute from "./util/AuthRoute";
@@ -35,7 +39,7 @@ if (token) {
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <div className="App">
+      <Provider store={store}>
         <Router>
           <Navbar></Navbar>
           <div className="container">
@@ -56,7 +60,7 @@ function App() {
             </Switch>
           </div>
         </Router>
-      </div>
+      </Provider>
     </MuiThemeProvider>
   );
 }
